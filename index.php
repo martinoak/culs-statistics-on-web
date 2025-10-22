9d26d28ba3aca6bb90ead40c22dc155aa8522d91
Martin <martin.dub@dek-cz.com>
2025-10-22 19:54:17 +0200

    UTF-8

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<html>
+ï»¿<html>
 <head>
 </head>
 <body bgcolor=navajowhite link=saddlebrown vlink=darkgoldenrod>
-<h3>TESTOVÁNÍ HYPOTÉZ:</h3> <a href="celekTEST.php"> 
+<h3>TESTOVÃNÃ HYPOTÃ‰Z:</h3> <a href="celekTEST.php"> 
 <img id="cz" src="cz.png" width="100" height="40"> 
 </a>
 <h3>HYPOTHESIS TESTING:</h3> <a href="EcelekTEST.php"> 
 <img id="cz" src="gb.png" width="100" height="40"> 
 </a>
 <br><br><br><br><br><br><br>
-&copy; František Mošna 
-<br> Èeská Zemìdìlská Univ. v Praze, Karlova Univ. v Praze, Pedagogická fak., Èeská rep. 
+&copy; FrantiÅ¡ek MoÅ¡na 
+<br> ÄŒeskÃ¡ ZemÄ›dÄ›lskÃ¡ Univ. v Praze, Karlova Univ. v Praze, PedagogickÃ¡ fak., ÄŒeskÃ¡ rep. 
 <br> Czech Univ. of Life Sciences in Prague, Charles Univ. in Prague, Fac. of Education,  Czech rep.
 </body>
 </html>
